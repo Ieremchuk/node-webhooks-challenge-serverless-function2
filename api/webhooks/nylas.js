9250d6be42c07fec08616763a2f29ed82ce383d5
Oleksandr Ieremchuk <Ieremchuk@users.noreply.github.com>
2024-07-20 17:48:24 -0500

    Update nylas.js

--- api/webhooks/nylas.js
+++ api/webhooks/nylas.js
@@ -21,7 +21,7 @@ export default function handler(request, response) {
     const options = {
       hostname: 'hooks.zapier.com',
       port: 443,
-      path: '/hooks/catch/2303567/22gnygp/',
+      path: '/hooks/catch/2303567/22qb0qs/',
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
